ce214d2438af8f38d1a9c7e44e746bbdeb563978
kairuanck <97305511+kairuanck@users.noreply.github.com>
2026-03-21 11:48:51 -0300

    Refactor index.tsx to include CSS import

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -6,7 +7,6 @@ const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
-
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
